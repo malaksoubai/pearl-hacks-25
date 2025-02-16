11c3d361680f03211300d31a288b1a55307db53e
Allana Adams <adamsallana1@gmail.com>
2025-02-16 01:40:48 -0500

    added front-end design in myapp folder

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Route to render the HTML page
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index'); // Render the 'index.ejs' file
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
