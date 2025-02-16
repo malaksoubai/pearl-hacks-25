addbcc7c50fa3b57e5fa8f3d0462789477cf3637
niveditaravishankar <nravish1@unc.edu>
2025-02-16 02:41:30 -0500

    Final Commit

---
+++ folder/InterviewDemo/run_scripts.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.run(["python", "/Users/niveditaravishankar/Eye-Strain-Detection/pearl-hacks-25/folder/InterviewDemo/interview_cam.py"])
+
+subprocess.run(["python", "/Users/niveditaravishankar/Eye-Strain-Detection/pearl-hacks-25/folder/InterviewDemo/apicall.py"])
+
+
